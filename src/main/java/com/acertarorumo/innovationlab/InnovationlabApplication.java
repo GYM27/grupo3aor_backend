b9dd948eb34d26fff94614e811a8f508760a50f6
GYM27 <oluis9@gmail.com>
2026-05-25 17:11:25 +0100

    Initialize Spring Boot skeleton project

---
+++ src/main/java/com/acertarorumo/innovationlab/InnovationlabApplication.java
@@ -0,0 +1,13 @@
+package com.acertarorumo.innovationlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovationlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovationlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acertarorumo/innovationlab/InnovationlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acertarorumo.innovationlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovationlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
